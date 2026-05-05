d2bc448ffe3152fdad410e3cf95996360d9ee345
komzo404 <ismoilovruslan404@gmail.com>
2026-05-05 12:09:59 +0500

    ss

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const express=require('express')
-
+const {getUSer,postUSer}=require('./controllers/user.controler')
+const  mongoose  = require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
 const app=express()
 const users=[{"id":1,"name":"Ruslan"},{"id":2,"name":"Firdsv"}]
 app.use(express.json())
-app.get('/users',(req,res)=>{
-    res.send(users )
-})
-app.get('/users/:userid',(req,res)=>{
-    const userID=req.params.userid
-    const findUser=users.find((item)=>item.id==userID)
-    res.send(findUser)
-})
-app.post('/add',(req,res)=>{
-    console.log(req.body);
-    users.push(req.body)
-    res.send({succes:true})
-})
+app.get('/users',getUSer)
+app.post('/add',postUSer)
 app.delete('/delete/:id', (req, res) => {
     const userID = req.params.id
     const flterUser = users.filter((user) => user.id !== userID)
     res.send({ success: true, users: flterUser })
 })
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Bazaga ulandi'))
+  .catch(err => console.log('Xatolik:', err))
 app.put('/put/:id', (req, res) => {
     const userID = req.params.id
     const findUser=users.find((item)=>item.id==userID)
-    findUser.name='JAsur'
-    res.send({ success: true,messgae:"Profil ozgartirildi" })
+    findUser.name='JAsur' 
 })
 app.listen(3001,()=>{
     console.log('Backend 3001-portda ishladi');
